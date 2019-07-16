f51bffb4ccdbb0f0f7c60af0e70312da6fd8a9d2
david1297 <juandavid.andrade1997@gmail.com>
2019-07-16 14:32:48 -0500

    .

--- classes/Auditoria.php
+++ classes/Auditoria.php
@@ -1,7 +1,7 @@
 <?php
 function NuevoE($Tipo,$Numero){
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    $sql="SELECT * FROM  pedidoe  where Tipo ='$Tipo' and Numero = $Numero ";
+    $sql="SELECT * FROM  PEDIDOE  where Tipo ='$Tipo' and Numero = $Numero ";
     $query = mysqli_query($con, $sql);
     $row=mysqli_fetch_array($query);
     $Tipo = $row['Tipo'];
@@ -15,14 +15,14 @@ function NuevoE($Tipo,$Numero){
     $USERNAME = $row['USERNAME'];
     $IDVEND = $row['IDVEND'];
 
-	$sql =  "INSERT INTO Auditoria_pedidoE (Tipo,Numero,Id_N,succliente,Subtotal,Iva,Descuento,Comentario,USERNAME,IDVEND,Fecha,Hora,Operacion) VALUES 
+	$sql =  "INSERT INTO AUDITORIA_PEDIDOE (Tipo,Numero,Id_N,succliente,Subtotal,Iva,Descuento,Comentario,USERNAME,IDVEND,Fecha,Hora,Operacion) VALUES 
     ('$Tipo',$Numero,'$Id_N',$succliente,$Subtotal,$Iva,$Descuento,'$Comentario','".$_SESSION['USERNAME']."',$IDVEND,CURDATE(),curTime(),'Creacion');";
     $query_update = mysqli_query($con,$sql);
     echo $sql;
 }
 function EditarE($Tipo,$Numero,$Id_N){
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    $sql="SELECT * FROM  pedidoe  where Tipo ='$Tipo' and Numero = $Numero ";
+    $sql="SELECT * FROM  PEDIDOE  where Tipo ='$Tipo' and Numero = $Numero ";
     $query = mysqli_query($con, $sql);
     $row=mysqli_fetch_array($query);
     $Tipo = $row['Tipo'];
@@ -36,7 +36,7 @@ function EditarE($Tipo,$Numero,$Id_N){
     $USERNAME = $row['USERNAME'];
     $IDVEND = $row['IDVEND'];
 
-    $sql="SELECT * FROM  temp_pedidoe  where Id_N ='$Id_N'  ";
+    $sql="SELECT * FROM  TEMP_PEDIDOE  where Id_N ='$Id_N'  ";
     $query = mysqli_query($con, $sql);
     $row=mysqli_fetch_array($query);
     $Tipot = $row['Tipo'];
@@ -50,14 +50,14 @@ function EditarE($Tipo,$Numero,$Id_N){
     $USERNAMEt = $row['USERNAME'];
     $IDVENDt = $row['IDVEND'];
     if(($succlientet<>$succliente)|| ($Subtotalt<>$Subtotal)||($Ivat<>$Iva)||($Descuentot<>$Descuento)||($Comentariot<>$Comentario)||($IDVENDt<>$IDVEND) ){
-        $sql =  "INSERT INTO Auditoria_pedidoE (Tipo,Numero,Id_N,succliente,Subtotal,Iva,Descuento,Comentario,USERNAME,IDVEND,Fecha,Hora,Operacion) VALUES 
+        $sql =  "INSERT INTO AUDITORIA_PEDIDOE (Tipo,Numero,Id_N,succliente,Subtotal,Iva,Descuento,Comentario,USERNAME,IDVEND,Fecha,Hora,Operacion) VALUES 
         ('$Tipo',$Numero,'$Id_N',$succlientet,$Subtotalt,$Ivat,$Descuentot,'$Comentariot','".$_SESSION['USERNAME']."',$IDVEND,CURDATE(),curTime(),'Modificacion');";
         $query_update = mysqli_query($con,$sql);
     }   
 }
 function NuevoD($Tipo,$Numero){
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    $sql="SELECT * FROM pedidod  where Tipo ='$Tipo' and Numero = $Numero ";
+    $sql="SELECT * FROM PEDIDOD  where Tipo ='$Tipo' and Numero = $Numero ";
     $query = mysqli_query($con, $sql);
     while($row=mysqli_fetch_array($query)){
     $Id_N = $row['Id_N'];
@@ -74,7 +74,7 @@ function NuevoD($Tipo,$Numero){
     $Bodega = $row['Bodega'];
     $Tarifa = $row['Tarifa'];
     
-    $sql =  "INSERT INTO auditoria_pedidod (Tipo,Numero,Id_N,Item,Cantidad,Subtotal,Iva,Descuento,Bonificado,COMENTARIO,Precio,Bodega,Tarifa,USERNAME,Fecha,Hora,Operacion) VALUES 
+    $sql =  "INSERT INTO AUDITORIA_PEDIDOD (Tipo,Numero,Id_N,Item,Cantidad,Subtotal,Iva,Descuento,Bonificado,COMENTARIO,Precio,Bodega,Tarifa,USERNAME,Fecha,Hora,Operacion) VALUES 
         ('$Tipo',$Numero,'$Id_N','$Item',$Cantidad,$Subtotal,$Iva,$Descuento,'$Bonificado','$Comentario',$Precio,'$Bodega',$Tarifa,'".$_SESSION['USERNAME']."',CURDATE(),curTime(),'Creacion');";
         $query_update = mysqli_query($con,$sql);
     }    
@@ -82,7 +82,7 @@ function NuevoD($Tipo,$Numero){
 function EditarD($Tipo,$Numero,$Id_N){
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     
-    $sql="SELECT * FROM temp_pedidod where Id_N ='$Id_N'and Estado <>'-' ";
+    $sql="SELECT * FROM TEMP_PEDIDOD where Id_N ='$Id_N'and Estado <>'-' ";
     $query = mysqli_query($con, $sql);
     while($row=mysqli_fetch_array($query)){
         $Item = $row['Item'];
@@ -97,7 +97,7 @@ function EditarD($Tipo,$Numero,$Id_N){
         $Tarifa = $row['Tarifa'];
         $Estado = $row['Estado'];
 
-        $sql =  "INSERT INTO auditoria_pedidod (Tipo,Numero,Id_N,Item,Cantidad,Subtotal,Iva,Descuento,Bonificado,COMENTARIO,Precio,Bodega,Tarifa,USERNAME,Fecha,Hora,Operacion) VALUES 
+        $sql =  "INSERT INTO AUDITORIA_PEDIDOD (Tipo,Numero,Id_N,Item,Cantidad,Subtotal,Iva,Descuento,Bonificado,COMENTARIO,Precio,Bodega,Tarifa,USERNAME,Fecha,Hora,Operacion) VALUES 
         ('$Tipo',$Numero,'$Id_N','$Item',$Cantidad,$Subtotal,$Iva,$Descuento,'$Bonificado','$Comentario',$Precio,'$Bodega',$Tarifa,'".$_SESSION['USERNAME']."',CURDATE(),curTime(),'$Estado');";
         $query_update = mysqli_query($con,$sql);
     }
